e11e006d9508a088674717b7093defe989d7c1d8
Vinicius Spagnol <vinicius.spagnol@gmail.com>
2018-11-05 18:42:39 -0200

    first commit

--- README.markdown
+++ README.markdown
@@ -1,18 +1,12 @@
-# dotenv-safe
+# sq-dotenv
 
-Identical to [`dotenv`](https://github.com/motdotla/dotenv), but ensures that all necessary environment variables are defined after reading from `.env`.
+Identical to [`dotenv`](https://github.com/motdotla/dotenv) and [`dotenv-safe](https://github.com/motdotla/dotenv-safe), but ensures that all necessary environment variables are defined after reading from `.env` and `./kub/kub_deployment.yml.template`.
 These needed variables are read from `.env.example`, which should be commited along with your project.
-
-[![Build Status](https://travis-ci.org/rolodato/dotenv-safe.svg)](https://travis-ci.org/rolodato/dotenv-safe)
 
 # Installation
 
 ```
-npm install --save dotenv-safe
-```
-
-```
-yarn add dotenv-safe
+npm install --save github:squidit/sq-dotenv
 ```
 
 # Example
@@ -32,10 +26,10 @@ TOKEN=
 
 ```js
 // index.js
-require('dotenv-safe').config();
+require('sq-dotenv').config();
 ```
 
-Since the provided `.env` file does not contain all the variables defined in
+Since the provided `.env` or `./kub/kub_deployment.yml.template` file does not contain all the variables defined in
 `.env.example`, an exception is thrown:
 
 ```
@@ -44,9 +38,13 @@ MissingEnvVarsError: The following variables were defined in .env.example but ar
 Make sure to add them to .env or directly to the environment.
 
 If you expect any of these variables to be empty, you can use the allowEmptyValues option:
-require('dotenv-safe').config({
+require('sq-dotenv').config({
   allowEmptyValues: true
 });
+
+If you expect only one variable to be empty, only set in `.env.example` file the variable value to `empty` as:
+
+EMPTY=empty
 ```
 
 Not all the variables have to be defined in `.env`, they can be supplied externally.
@@ -61,7 +59,7 @@ $ TOKEN=abc KEY=xyz node index.js
 Requiring and loading is identical:
 
 ```js
-require('dotenv-safe').config();
+require('sq-dotenv').config();
 ```
 
 This will load environment variables from `.env` as usual, but will also read any variables defined in `.env.example`.
@@ -79,7 +77,7 @@ Otherwise, returns an object with the following format:
 
 If all the required variables were successfully read but an error was thrown when trying to read the `.env` file, the error will be included in the result object under the `error` key.
 
-`dotenv-safe` compares the actual environment after loading `.env` (if any) with the example file, so it will work correctly if environment variables are missing in `.env` but provided through other means such as a shell script.
+`sq-dotenv` compares the actual environment after loading `.env` (if any) with the example file, so it will work correctly if environment variables are missing in `.env` but provided through other means such as a shell script.
 
 ## Continuous integration (CI)
 
@@ -88,7 +86,7 @@ This can be done by checking if the `CI` environment variable is defined, which 
 For example:
 
 ```js
-require('dotenv-safe').config({
+require('sq-dotenv').config({
   example: process.env.CI ? '.env.ci.example' : '.env.example'
 });
 ```
@@ -98,7 +96,7 @@ require('dotenv-safe').config({
 [Same options and methods supported by `dotenv`](https://github.com/motdotla/dotenv#options).
 
 ```js
-require('dotenv-safe').config({
+require('sq-dotenv').config({
     allowEmptyValues: true,
     example: './.my-env-example-filename'
 });
--- index.js
+++ index.js
@@ -1,51 +1,60 @@
-'use strict';
+'use strict'
 
-const dotenv = require('dotenv');
-const fs = require('fs');
-const MissingEnvVarsError = require('./MissingEnvVarsError.js');
+const dotenv = require('dotenv')
+const yaml = require('js-yaml')
+const fs = require('fs')
+const get = require('lodash/get')
+const MissingEnvVarsError = require('./MissingEnvVarsError.js')
 
 function difference (arrA, arrB) {
-    return arrA.filter(a => arrB.indexOf(a) < 0);
+  return arrA.filter(a => arrB.indexOf(a[0]) < 0 && a[1] !== 'empty')
 }
 
 function compact (obj) {
-    const result = {};
-    Object.keys(obj).forEach(key => {
-        if (obj[key]) {
-            result[key] = obj[key];
-        }
-    });
-    return result;
+  const result = {}
+  Object.keys(obj).forEach(key => {
+    if (obj[key]) {
+      result[key] = obj[key]
+    }
+  })
+  return result
 }
 
 module.exports = {
-    config: function(options = {}) {
-        const dotenvResult = dotenv.load(options);
-        const example = options.example || options.sample || '.env.example';
-        const allowEmptyValues = options.allowEmptyValues || false;
-        const processEnv = allowEmptyValues ? process.env : compact(process.env);
-        const exampleVars = dotenv.parse(fs.readFileSync(example));
-        const missing = difference(Object.keys(exampleVars), Object.keys(processEnv));
+  config: function (options = {}) {
+    const dotenvResult = dotenv.load(options)
+    const kub = options.kub || './kub/kub_deployment.yml.template'
+    const envKub = get(yaml.safeLoad(fs.readFileSync(kub, 'utf8')), 'spec.template.spec.containers[0].env')
+    const example = options.example || options.sample || '.env.example'
+    const allowEmptyValues = options.allowEmptyValues || false
+    const processEnv = allowEmptyValues ? process.env : compact(process.env)
+    const exampleVars = dotenv.parse(fs.readFileSync(example))
+    const missingDotenv = difference(Object.entries(exampleVars), Object.keys(processEnv))
+    const missingKub = difference(Object.entries(exampleVars), envKub.map(e => e.name))
 
-        if (missing.length > 0) {
-            throw new MissingEnvVarsError(allowEmptyValues, options.path || '.env', example, missing, dotenvResult.error);
-        }
+    if (missingDotenv.length > 0) {
+      throw new MissingEnvVarsError(allowEmptyValues, options.path || '.env', example, missingDotenv, dotenvResult.error)
+    }
 
-        // Key/value pairs defined in example file and resolved from environment
-        const required = Object.keys(exampleVars).reduce((acc, key) => {
-            acc[key] = process.env[key];
-            return acc;
-        }, {});
-        const error = dotenvResult.error ? { error: dotenvResult.error } : {};
-        const result = {
-            parsed: dotenvResult.error ? {} : dotenvResult.parsed,
-            required: required
-        };
-        return Object.assign(result, error);
-    },
-    parse: dotenv.parse,
-    MissingEnvVarsError: MissingEnvVarsError
-};
+    if (missingKub.length > 0) {
+      throw new MissingEnvVarsError(allowEmptyValues, kub, example, missingKub, dotenvResult.error)
+    }
 
-module.exports.load = module.exports.config;
-module.exports.MissingEnvVarsError = MissingEnvVarsError;
+    // Key/value pairs defined in example file and resolved from environment
+    const required = Object.keys(exampleVars).reduce((acc, key) => {
+      acc[key] = process.env[key]
+      return acc
+    }, {})
+    const error = dotenvResult.error ? { error: dotenvResult.error } : {}
+    const result = {
+      parsed: dotenvResult.error ? {} : dotenvResult.parsed,
+      required: required
+    }
+    return Object.assign(result, error)
+  },
+  parse: dotenv.parse,
+  MissingEnvVarsError: MissingEnvVarsError
+}
+
+module.exports.load = module.exports.config
+module.exports.MissingEnvVarsError = MissingEnvVarsError
